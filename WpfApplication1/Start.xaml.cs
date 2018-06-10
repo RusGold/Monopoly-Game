1d7e35a04b0b280922f3a8d3d0dcfa7fde7c2d46
Rustam <32103548+RusGold@users.noreply.github.com>
2018-06-10 22:38:48 +0300

    Update Start.xaml.cs

--- WpfApplication1/Start.xaml.cs
+++ WpfApplication1/Start.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,42 @@ namespace WpfApplication1
     /// </summary>
     public partial class Start : Window
     {
+        int NumberOfImage = 0;
+        List<Image> Images = new List<Image>();
+
         public Start()
         {
             InitializeComponent();
+            List<int> NumberOfPlayers = new List<int>() { 1, 2, 3 };
+            PlayersBox.ItemsSource = NumberOfPlayers;
+            DirectoryInfo Faces = new DirectoryInfo("WpfApplication1/faces");
+            foreach (var face in Faces.EnumerateFiles("*.png"))
+                Images.Add(Image.FromFile(face.FullName)); // problem with uploading images (FromFile is not read)
+            AvatarImage.Content = Images[NumberOfImage];
+            Dictionary<string, object> Colours = new Dictionary<string, object>();
+            // do like Tolya said
+            ColoursBox.ItemsSource = Colours.Keys; // select key => select object (colour)
+            // how add a colour to dicrtionary ?!?!?!
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NumberOfImage != 0)
+                AvatarImage.Content = Images[NumberOfImage - 1];
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NumberOfImage != Images.LastIndexOf(Images.Last()))
+                AvatarImage.Content = Images[NumberOfImage + 1];
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // player.face = AvatarImage.Content
+            // player.name = NicknameBox.Text
+            // player.colour = take a key from ColourBox.Item => find an object by key in Dic Colours => select a value (colour)
+            // give information about number of players to game from PlayersBox.Item
         }
     }
 }
