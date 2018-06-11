a7d4f9fc8a64a4e3a84464c6eb2f2616761e79b5
Rustam <32103548+RusGold@users.noreply.github.com>
2018-06-11 23:50:26 +0300

    Update Start.xaml.cs

--- WpfApplication1/Start.xaml.cs
+++ WpfApplication1/Start.xaml.cs
@@ -23,20 +23,56 @@ namespace WpfApplication1
     {
         int NumberOfImage = 0;
         List<Image> Images = new List<Image>();
+        
+        private Color SetTheColor(string s)
+        {
+            if (s == "Yellow")
+                return (Color)ColorConverter.ConvertFromString("#ffff00");
+            if (s == "Green")
+                return (Color)ColorConverter.ConvertFromString("#008000");
+            if (s == "Red")
+                return (Color)ColorConverter.ConvertFromString("#ff0000");
+            if (s == "Black")
+                return (Color)ColorConverter.ConvertFromString("#000000");
+            if (s == "Blue")
+                return (Color)ColorConverter.ConvertFromString("#42aaff");
+            if (s == "Purple")
+                return (Color)ColorConverter.ConvertFromString("#800080");
+            if (s == "Orange")
+                return (Color)ColorConverter.ConvertFromString("#ffa500");
+            if (s == "White")
+                return (Color)ColorConverter.ConvertFromString("#ffffff");
+            if (s == "Pink")
+                return (Color)ColorConverter.ConvertFromString("#ffc0cb");
+            if (s == "Coral")
+                return (Color)ColorConverter.ConvertFromString("#ff7f50");
+            if (s == "Dark blue")
+                return (Color)ColorConverter.ConvertFromString("#002137");
+            if (s == "Dark green")
+                return (Color)ColorConverter.ConvertFromString("#013220");
+            return (Color)ColorConverter.ConvertFromString("#ffffff"); 
+        }
 
         public Start()
         {
             InitializeComponent();
+
             List<int> NumberOfPlayers = new List<int>() { 1, 2, 3 };
             PlayersBox.ItemsSource = NumberOfPlayers;
+
             DirectoryInfo Faces = new DirectoryInfo("WpfApplication1/faces");
             foreach (var face in Faces.EnumerateFiles("*.png"))
-                Images.Add(Image.FromFile(face.FullName)); // problem with uploading images (FromFile is not read)
+            {
+                Image i = new Image
+                {
+                    Source = new BitmapImage(new Uri("pack://application:,,,/WpfApplication1;component/Resources/" + face.FullName))
+                };
+                Images.Add(i);
+            }
             AvatarImage.Content = Images[NumberOfImage];
-            Dictionary<string, object> Colours = new Dictionary<string, object>();
-            // do like Tolya said
-            ColoursBox.ItemsSource = Colours.Keys; // select key => select object (colour)
-            // how add a colour to dicrtionary ?!?!?!
+
+            List<string> NameOfColours = new List<string>() {"Yellow", "Green", "Red", "Black", "Blue", "Purple", "Orange", "White", "Pink", "Coral", "Dark blue", "Dark green" };
+            ColoursBox.ItemsSource = NameOfColours;
         }
 
         private void PreviousButton_Click(object sender, RoutedEventArgs e)
@@ -53,10 +89,8 @@ namespace WpfApplication1
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            // player.face = AvatarImage.Content
-            // player.name = NicknameBox.Text
-            // player.colour = take a key from ColourBox.Item => find an object by key in Dic Colours => select a value (colour)
-            // give information about number of players to game from PlayersBox.Item
+            Player User = new Player(NicknameBox.Text, SetTheColor(ColoursBox.SelectedItem.ToString())) { face = (Image)AvatarImage.Content};
+            // give information about number of players to game from PlayersBox.Item (where?)
         }
     }
 }
