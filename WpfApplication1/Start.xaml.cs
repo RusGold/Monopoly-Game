40a083fa3268b1b23cf82f0736373a87c6660341
MotherRussia <39841955+MotherRussia00@users.noreply.github.com>
2018-06-12 23:48:21 +0300

    Update Start.xaml.cs

--- WpfApplication1/Start.xaml.cs
+++ WpfApplication1/Start.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Drawing;
 using System.Linq;
@@ -13,17 +14,31 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfApplication1.Data;
 
 namespace WpfApplication1
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
+    /// <summary> 
+    /// Interaction logic for Window1.xaml 
+    /// </summary> 
     public partial class Start : Window
     {
         int NumberOfImage = 0;
+        //private static CommonData cdata = MainWindow.cdata;
         List<Image> Images = new List<Image>();
-        
+        public ObservableCollection<lPlayer> pAll { get; set; }
+        List<string> NameOfColours = new List<string>() { "Yellow", "Green", "Red", "Black", "Blue", "Purple", "Orange", "White", "Pink", "Coral", "Dark blue", "Dark green" };
+
+        //
+
+        public class lPlayer
+        {
+            public int num { get; set; }
+            public string name { get; set; }
+            public string color { get; set; }
+            public Image face { get; set; }
+        }
+
         private Color SetTheColor(string s)
         {
             if (s == "Yellow")
@@ -50,47 +65,95 @@ namespace WpfApplication1
                 return (Color)ColorConverter.ConvertFromString("#002137");
             if (s == "Dark green")
                 return (Color)ColorConverter.ConvertFromString("#013220");
-            return (Color)ColorConverter.ConvertFromString("#ffffff"); 
+            return (Color)ColorConverter.ConvertFromString("#ffffff");
         }
 
         public Start()
         {
             InitializeComponent();
-
-            List<int> NumberOfPlayers = new List<int>() { 1, 2, 3 };
-            PlayersBox.ItemsSource = NumberOfPlayers;
-
-            DirectoryInfo Faces = new DirectoryInfo("WpfApplication1/faces");
-            foreach (var face in Faces.EnumerateFiles("*.png"))
+            pAll = new ObservableCollection<lPlayer>();
+            AllPlayers.ItemsSource = pAll;
+            DirectoryInfo faces = new DirectoryInfo("../../faces");
+            foreach (var face in faces.EnumerateFiles("*.png"))
             {
                 Image i = new Image
                 {
-                    Source = new BitmapImage(new Uri("pack://application:,,,/WpfApplication1;component/Resources/" + face.FullName))
+                    Source = new BitmapImage(new Uri("pack://application:,,,/WpfApplication1;component/Resources/" + face.Name))
                 };
                 Images.Add(i);
             }
             AvatarImage.Content = Images[NumberOfImage];
 
-            List<string> NameOfColours = new List<string>() {"Yellow", "Green", "Red", "Black", "Blue", "Purple", "Orange", "White", "Pink", "Coral", "Dark blue", "Dark green" };
+
             ColoursBox.ItemsSource = NameOfColours;
         }
 
         private void PreviousButton_Click(object sender, RoutedEventArgs e)
         {
-            if (NumberOfImage != 0)
+            if (NumberOfImage > 0)
+            {
                 AvatarImage.Content = Images[NumberOfImage - 1];
+                NumberOfImage--;
+            }
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            if (NumberOfImage != Images.LastIndexOf(Images.Last()))
+            if (NumberOfImage < Images.LastIndexOf(Images.Last()))
+            {
                 AvatarImage.Content = Images[NumberOfImage + 1];
+                NumberOfImage++;
+            }
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            Player User = new Player(NicknameBox.Text, SetTheColor(ColoursBox.SelectedItem.ToString())) { face = (Image)AvatarImage.Content};
-            // give information about number of players to game from PlayersBox.Item (where?)
+            if (pAll.Count < 2)
+            {
+                MessageBox.Show("Нужно играть хотя бы вдвоём!");
+            }
+            else
+            {
+                //replace by Starter window logic
+                MainWindow.cdata.players = new Player[pAll.Count];
+                var i = 0;
+                foreach (lPlayer l in pAll)
+                {
+                    Player tPl = new Player(i,"pl" + Convert.ToString(i) + "1", SetTheColor(l.color), l.face);
+                    MainWindow.cdata.players[i] = tPl;
+                    MainWindow.cdata.players[i].name = l.name+" (Player " + Convert.ToString(i + 1)+")";
+                    var b = (Ellipse)MainWindow.cdata.wnd.Board.FindName(MainWindow.cdata.players[i].code+"1");
+                    b.Fill = new SolidColorBrush(tPl.c);
+                    b.Visibility = Visibility.Visible;
+                    b = (Ellipse)MainWindow.cdata.wnd.Board.FindName(MainWindow.cdata.players[i].code + "0");
+                    b.Visibility = Visibility.Visible;
+                    i++;
+                }
+                ((Image)MainWindow.cdata.wnd.FindName("portrait")).Source = MainWindow.cdata.players[0].face.Source;
+                MainWindow.cdata.cPlayer = MainWindow.cdata.players[0];
+                MainWindow.cdata.wnd.PInfo.Text = MainWindow.cdata.cPlayer.name + Convert.ToChar(13) + Convert.ToString(MainWindow.cdata.cPlayer.budget);
+                //////////////////////////////////////////////////////////////////////////////////
+                Close();
+            }
+
+        }
+
+        private void add_click(object sender, RoutedEventArgs e)
+        {
+            if (ColoursBox.SelectedItem != null)
+            {
+                pAll.Add(new lPlayer { num = pAll.Count + 1, name = NicknameBox.Text, color = ColoursBox.Text, face = Images[NumberOfImage] });
+                var i = NameOfColours.FindIndex(x => x == ColoursBox.Text);
+                NameOfColours.RemoveAt(i);
+                ColoursBox.Items.Refresh();
+                ColoursBox.SelectedItem = null;
+                if (pAll.Count > 1) bStart.ToolTip = null;
+                if (pAll.Count > 3) Add.IsEnabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Нельзя играть бесцветной фишкой :)");
+            }
         }
     }
 }
