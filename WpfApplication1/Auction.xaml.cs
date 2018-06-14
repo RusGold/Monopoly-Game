9109922627f341bbe5fe1c8c51a859a1c4fcf8a0
MotherRussia <39841955+MotherRussia00@users.noreply.github.com>
2018-06-14 23:43:27 +0300

    Update Auction.xaml.cs

--- WpfApplication1/Auction.xaml.cs
+++ WpfApplication1/Auction.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,52 +19,13 @@ namespace WpfApplication1
     /// </summary>
     public partial class Auction : Window
     {
-        private List<Player> Comp = new List<Player>();
-        private Int32 currentBet = 10;
-        private Player fp;
+        public List<Player> Comp = new List<Player>();
+        public Int32 currentBet = 10;
+        public Player fp;
         public Auction()
         {
             InitializeComponent();
-            foreach (Player p in MainWindow.cdata.players)
-            {
-                if (p == MainWindow.cdata.cPlayer)
-                {
-                    Log.Items.Add(p.name + " , начальная ставка: 10");
-                }
-                else
-                {
-                    if (p.isalive && p.budget>=currentBet) Comp.Add(p);
-                }
-
-            }
-            foreach (Player c in Comp)
-            {
-                var s = "comp" + Convert.ToString(Comp.IndexOf(c)+1);
-                var v = (Rectangle)FindName(s);
-                v.Fill = new SolidColorBrush(c.c);
-                v.Visibility = Visibility.Visible;
-            }
-            fp = Comp.First();
-            cComp.Fill = new SolidColorBrush(fp.c);
-            var prt = (Image)FindName("portrait");
-            prt.Source = fp.face.Source;
-            PInfo.Content = fp.name + ", " + Convert.ToString(fp.budget);
-            if (Comp.Count == 1)
-            {
-                var yyy = MainWindow.cdata.cells[MainWindow.cdata.cPlayer.position];
-                var gPlayer = MainWindow.cdata.players[fp.playerid];
-                yyy.prop.ownerplayer = gPlayer;
-                gPlayer.budget -= currentBet;
-                gPlayer.property.Add(yyy.prop);
-                var str = "p" + Convert.ToString(yyy.y) + Convert.ToString(yyy.x) + "4";
-                Rectangle r = (Rectangle)MainWindow.cdata.wnd.Board.FindName(str);
-                r.Fill = new SolidColorBrush(gPlayer.c);
-                if (gPlayer.HasMono(yyy.card.color))
-                {
-                    gPlayer.mono.Add(yyy.card.color);
-                };
-                Hide();
-            }
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -92,14 +53,21 @@ namespace WpfApplication1
                     if (c.budget < currentBet)
                     {
                         Log.Items.Add(c.name+ " заканчивает аукцион: мало средств");
-                        var s = "comp" + Convert.ToString(Comp.IndexOf(c)+1);
+                        var d = Comp.Count();
+                        var s = "comp" + Convert.ToString(d);
                         var b = (Rectangle)FindName(s);
                         b.Visibility = Visibility.Hidden;
                         c.isalive = false;
                     }          
                 }
                 Comp.RemoveAll(x => x.isalive == false);
-                if (Comp.Count==1)
+                foreach (Player c in Comp)
+                {
+                    var s = "comp" + Convert.ToString(Comp.IndexOf(c)+1);
+                    var b = (Rectangle)FindName(s);
+                    b.Fill = new SolidColorBrush(c.c);
+                }
+                if (Comp.Count<2)
                 {
                     var yyy = MainWindow.cdata.cells[MainWindow.cdata.cPlayer.position];
                     var gPlayer = MainWindow.cdata.players[fp.playerid];
@@ -113,7 +81,8 @@ namespace WpfApplication1
                     {
                         gPlayer.mono.Add(yyy.card.color);
                     };
-                    Hide();
+                    MainWindow.cdata.wnd.MainLog.Items.Insert(0, gPlayer.name + " купил " + yyy.prop.name + " за " + currentBet + ". Оставшийся бюджет " + Convert.ToString(gPlayer.budget) + ".");
+                    Close();
                 }
                 var ind = Comp.IndexOf(fp);
                 if (ind+1 == Comp.Count)
@@ -121,6 +90,112 @@ namespace WpfApplication1
                     fp = Comp.First();
                 }
                 else { fp = Comp[Comp.IndexOf(fp) + 1]; }
+
+                cComp.Fill = new SolidColorBrush(fp.c);
+                var prt = (Image)FindName("portrait");
+                prt.Source = fp.face.Source;
+                PInfo.Content = fp.name + ", " + Convert.ToString(fp.budget);
+            }
+        }
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            var c = Comp.Find(x=>x.playerid==fp.playerid);
+            //MainWindow.cdata.wnd.Main
+            Log.Items.Add(c.name + " выходит из аукциона по доброй воле");
+            var s = "comp" + Convert.ToString(Comp.Count());//IndexOf(c) + 1);
+            var b = (Rectangle)FindName(s);
+            b.Visibility = Visibility.Hidden;
+            c.isalive = false;
+            Comp.RemoveAll(x => x.isalive == false);
+            foreach (Player comp in Comp)
+            {
+                s = "comp" + Convert.ToString(Comp.IndexOf(comp)+1);
+                b = (Rectangle)FindName(s);
+                b.Fill = new SolidColorBrush(comp.c);
+            }
+            var ind = Comp.IndexOf(fp);
+            if (ind + 1 == Comp.Count)
+            {
+                fp = Comp.First();
+            }
+            else { fp = Comp[Comp.IndexOf(fp) + 1]; }
+            cComp.Fill = new SolidColorBrush(fp.c);
+            var prt = (Image)FindName("portrait");
+            prt.Source = fp.face.Source;
+            PInfo.Content = fp.name + ", " + Convert.ToString(fp.budget);
+
+            if (Comp.Count < 2)
+            {
+                var yyy = MainWindow.cdata.cells[MainWindow.cdata.cPlayer.position];
+                var gPlayer = MainWindow.cdata.players[fp.playerid];
+                yyy.prop.ownerplayer = gPlayer;
+                gPlayer.budget -= currentBet;
+                gPlayer.property.Add(yyy.prop);
+                var str = "p" + Convert.ToString(yyy.y) + Convert.ToString(yyy.x) + "4";
+                Rectangle r = (Rectangle)MainWindow.cdata.wnd.Board.FindName(str);
+                r.Fill = new SolidColorBrush(gPlayer.c);
+                if (gPlayer.HasMono(yyy.card.color))
+                {
+                    gPlayer.mono.Add(yyy.card.color);
+                };
+                MainWindow.cdata.wnd.MainLog.Items.Insert(0, gPlayer.name + " купил " + yyy.prop.name + " за " + currentBet + ". Оставшийся бюджет " + Convert.ToString(gPlayer.budget) + ".");
+                Close();
+            }
+        }
+
+        public void AU_Init()
+        {
+            Log.Items.Clear();
+            Comp.Clear();
+            currentBet = 10;
+            foreach (Player p in MainWindow.cdata.players)
+            {
+                if (p == MainWindow.cdata.cPlayer)
+                {
+                    Log.Items.Add(p.name + " , начальная ставка: 10");
+                }
+                else
+                {
+                    if (p.isalive && p.budget >= currentBet) Comp.Add(new Player(p.playerid,p.code,p.c,p.face) {name=p.name,budget=p.budget,isalive=true,mono=p.mono,property=p.property,position=p.position } );
+                }
+
+            }
+            if (Comp.Count == 0)
+            {
+                MainWindow.cdata.wnd.MainLog.Items.Insert(0, "Аукцион не состоялся: у игроков недостаточно средств");
+
+                Close();
+
+            }
+            else
+            {
+                fp = Comp.First();
+                if (Comp.Count == 1)
+                {
+                    var yyy = MainWindow.cdata.cells[MainWindow.cdata.cPlayer.position];
+                    var gPlayer = MainWindow.cdata.players[fp.playerid];
+                    yyy.prop.ownerplayer = gPlayer;
+                    gPlayer.budget -= currentBet;
+                    gPlayer.property.Add(yyy.prop);
+                    var str = "p" + Convert.ToString(yyy.y) + Convert.ToString(yyy.x) + "4";
+                    Rectangle r = (Rectangle)MainWindow.cdata.wnd.Board.FindName(str);
+                    r.Fill = new SolidColorBrush(gPlayer.c);
+                    if (gPlayer.HasMono(yyy.card.color))
+                    {
+                        gPlayer.mono.Add(yyy.card.color);
+                    };
+                    MainWindow.cdata.wnd.MainLog.Items.Insert(0, "Аукцион не состоялся: только " + Comp.First().name + " имеет достаточно средств");
+                    MainWindow.cdata.wnd.MainLog.Items.Insert(0, Comp.First().name + " купил " + yyy.prop.name + " за " + currentBet + ". Оставшийся бюджет " + Convert.ToString(gPlayer.budget) + ".");
+                    Close();
+                }
+                foreach (Player c in Comp)
+                {
+                    var s = "comp" + Convert.ToString(Comp.IndexOf(c) + 1);
+                    var v = (Rectangle)FindName(s);
+                    v.Fill = new SolidColorBrush(c.c);
+                    v.Visibility = Visibility.Visible;
+                }
 
                 cComp.Fill = new SolidColorBrush(fp.c);
                 var prt = (Image)FindName("portrait");
