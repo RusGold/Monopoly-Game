9541d81ec4fa22e33449582afce4245965e6ffc5
MotherRussia <39841955+MotherRussia00@users.noreply.github.com>
2018-06-14 23:45:09 +0300

    Update Cell.cs

--- WpfApplication1/Cell.cs
+++ WpfApplication1/Cell.cs
@@ -1,21 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApplication1.Data;
 
 namespace WpfApplication1
 {
+    public enum cellType {jail, random, transport, service, country,special};
     public class Cell
     {
         public int num;
         public int x;
         public int y;
+        public cellType type;
         public Card card;
+        public string textDescr;
+        public Property prop;
+        public CardStack eff;
         public Cell()
         {
             card = new Card();
             num = x = y = -1;
+            textDescr = "";
+            prop = new Property();
         }
     }
 }
